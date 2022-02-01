d115aeb7e13f8b83ae30a218b6d1e0d86215ce85
Alejandro <aledelarosa2@gmail.com>
2022-02-01 13:12:56 +0100

    Nuevo cambio

--- ShoppingCartBonsai/src/test/java/org/rubiks/com/ShoppingCartBonsai/ShoppingCartTest.java
+++ ShoppingCartBonsai/src/test/java/org/rubiks/com/ShoppingCartBonsai/ShoppingCartTest.java
@@ -11,18 +11,20 @@ import org.junit.jupiter.api.Test;
 class ShoppingCartTest {
 	
 		private static ShoppingCart sc;
-		private static Product p;
+		private static Product a;
+		private static Product b;
 
 	@BeforeAll
 	static void iniciarCarrito() {
 		
-		ShoppingCart sc = new ShoppingCart();
-		Product a = new Product("Avion",2000.00);
-		Product b = new Product("Burro",30.0);
+		 sc = new ShoppingCart();
+		 a = new Product("Avion",2000.00);
+		 b = new Product("Burro",30.0);
 		
 	}
-
-	@AfterAll
+	
+	@AfterEach
+	
 	static void finish() {
 		
 		sc = null;
@@ -31,15 +33,76 @@ class ShoppingCartTest {
 
 	
 	
+
+	
+	
 	
 	@Test
-	
 	void addItem() {
-		sc.addItem(a);
+		
+		Product b = new Product("Tele",34.0);
+		sc.addItem(b);
 		
 		assertEquals(1,sc.getItemCount() );
 		
 		
+		
+		
+	}
+	
+	@Test
+	void removeItem() {
+		sc.addItem(a);
+		
+		try {
+			sc.removeItem(a);
+			sc.removeItem(b);
+
+			
+		}
+			
+		catch(ProductNotFoundException e) {
+			
+			fail();
+			
+		}
+		
+		assertEquals(0,sc.getItemCount() );
+
+		
+		
+	}
+	
+	@Test
+	void testGetBalance() {
+		
+		sc.addItem(a);
+		sc.addItem(b);
+		
+		assertEquals(2030.00,sc.getBalance(),"error");
+		
+		
+	}
+	
+	@Test
+	void testGetItemCount() {
+		
+		sc.addItem(a);
+		sc.addItem(b);
+		
+		assertEquals(2,sc.getItemCount(),"error");
+		
+		
+		
+	}
+	
+	@Test
+	void testEmpty() {
+		
+		sc.addItem(a);
+		sc.empty();
+		
+		assertEquals(0, sc.getItemCount());
 		
 		
 	}
